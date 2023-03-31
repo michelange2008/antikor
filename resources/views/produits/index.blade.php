5dcd34268a66d8c08d986eb4acede5480724f624
michelange2008 <laravel>
2023-03-31 07:08:20 +0200

    boutons pour choix type produit

--- resources/views/produits/index.blade.php
+++ resources/views/produits/index.blade.php
@@ -2,13 +2,22 @@
 
     <x-titre :texte="'liste_produits'" :icone="'produits.svg'"></x-titre>
 
-    <ul>
+    <ul class="grid grid-flow-row  grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-1 md:gap-2 xl:gap-3 ">
         @foreach ($phytotypes as $phytotype)
 
-        <li>{{$phytotype->name}}</li>
+        <li class="flex flex-row justify-start items-center  p-2 xl:p-3 bg-slate-300 hover:bg-slate-700 hover:text-stone-200">
+                <img class="w-8" src="{{ url('storage/img/produits/'.$phytotype->icone)}}" alt="{{$phytotype->name}}">
+                <p class="ml-1 md:ml-2 xl:ml-3">{{$phytotype->name}}</p>
+        </li>
             
         @endforeach
-    </ul>
+
+        <li class="flex flex-row justify-start items-center  p-2 xl:p-3 bg-slate-300 hover:bg-slate-700 hover:text-stone-200">
+            <img class="w-8" src="{{ url('storage/img/produits/fait.svg')}}" alt="tous">
+            <p class="ml-1 md:ml-2 xl:ml-3">Tout</p>
+    </li>
+        
+</ul>
 
     <ul class="mx-2">
 
