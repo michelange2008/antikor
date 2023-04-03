fe03dece4af4157ea0fa2bd4e685b700a5a0ef30
michelange2008 <laravel>
2023-04-03 19:50:15 +0200

    run build

--- resources/views/livewire/produit-create.blade.php
+++ resources/views/livewire/produit-create.blade.php
@@ -2,7 +2,7 @@
 
     <x-button-add></x-button-add>
 
-    <div x-show="open" class="fixed  w-8/12 sm:w-1/2 top-1/3 left-1/4 shadow-lg">
+    <div x-show="open" class="fixed  w-11/12 sm:w-1/2 top-1/3 left-2 sm:left-1/4 shadow-lg">
 
         <div class="flex flex-row items-center p-3 bg-teal-900">
             <img class="w-8 mr-2" src="{{url('storage/img/fonticone/add.svg')}}" alt="Add">
