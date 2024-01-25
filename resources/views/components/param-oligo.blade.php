71bc461d87592238163b9373af0a00ee50ce46c6
Michel BOUY <michelange@wanadoo.fr>
2024-01-25 22:05:08 +0100

    tap ?

--- resources/views/components/param-oligo.blade.php
+++ resources/views/components/param-oligo.blade.php
@@ -1,4 +1,4 @@
-    <div id="{{ $val }}" wire:click = "toggle('{{ $parametre }}','{{ $val }}')"
+    <div type="button" id="{{ $val }}" wire:click = "toggle('{{ $parametre }}','{{ $val }}')"
         class="my-2 text-center cursor-pointer py-2 px-2 md:py-3 md:px-4 
         bg-gray-300 shadow shadow-gray-800 active:bg-gray-800 active:text-white text-sm md:text-base
     @if ($param == $val) bg-teal-800 text-white shadow-teal-900 @endif"
