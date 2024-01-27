a36c1df740d754b63576b45c3559bb6f339b9608
michelange2008 <laravel>
2024-01-27 17:49:21 +0100

    modif mineurs mise en page

--- resources/views/livewire/oligos.blade.php
+++ resources/views/livewire/oligos.blade.php
@@ -1,13 +1,13 @@
 <div>
 
-    <h1 class="h2 mt-3">Que vaut votre complément minéral ?</h1>
+    <h1 class="mt-3 h2">Que vaut votre complément minéral ?</h1>
 
     <div class="flex flex-col lg:flex-row lg:gap-5">
 
-        <div class="flex my-3 flex-col gap-2 lg:flex-auto">
+        <div class="flex flex-col gap-2 my-3 lg:flex-auto">
 
             <div id="atelier" class="mb-6">
-                <h3 class="h3 pl-2 py-1 lg:pl-0 lg:text-center bg-gray-300 text-teal-900">1 - Choisir le type de
+                <h3 class="py-1 pl-2 text-teal-900 bg-gray-300 h3 lg:pl-0 lg:text-center">1 - Choisir le type de
                     production
                 </h3>
                 <div class="flex flex-row gap-2 justify-start lg:justify-center">
@@ -42,9 +42,9 @@
             </div>
             <div id="stade" class="mb-6">
                 <div id="stade_chevre">
-                    <h3 class="h3 pl-2 py-1 lg:pl-0 lg:text-center bg-gray-300 text-teal-900">2 - Choisir le stade
+                    <h3 class="py-1 pl-2 text-teal-900 bg-gray-300 h3 lg:pl-0 lg:text-center">2 - Choisir le stade
                         physiologique</h3>
-                    <div class="flex flex-row justify-start lg:justify-center gap-2">
+                    <div class="flex flex-row gap-2 justify-start lg:justify-center">
 
                         <div class="flex-auto">
                             @include('components.param-oligo', [
@@ -63,23 +63,22 @@
                                 'valeur' => 'Lactation',
                             ])
                         </div>
-                        <div class="flex-auto text-center px-1 md:px-2 lg:px-4">
-                            <div id="msi">
-                                <h3 class="lg:h3 my-1 text-sm md:text-base ">Ingestion</h3>
-                                <p class="text-sm md:text-base font-bold">{{ $msi }} kg/MS par jour</p>
-                            </div>
+                        <div id="msi"
+                            class="flex flex-col flex-auto justify-center px-1 my-2 text-center bg-gray-200 md:px-2 lg:px-4">
+                            <h3 class="mb-0 text-sm lg:h3 md:text-base">Ingestion</h3>
+                            <p class="mb-0 text-sm font-bold md:text-base">{{ $msi }} kg/MS par jour</p>
                         </div>
                     </div>
                 </div>
             </div>
             <div id="quantite">
-                <h3 class="h3 pl-2 py-1 lg:pl-0 lg:text-center bg-gray-300 text-teal-900">3 - Choisir la quantité
+                <h3 class="py-1 pl-2 text-teal-900 bg-gray-300 h3 lg:pl-0 lg:text-center">3 - Choisir la quantité
                     distribuée
                     <span class="inline md:hidden">(g/jour par animal)</span>
                 </h3>
-                <div class="text-center bg-teal-800 text-white py-1">
+                <div class="py-1 my-2 text-center text-white bg-teal-800">
                     <input
-                        class=" text-teal-900 md:text-lg lg:text-xl inline-block w-32 text-center rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"
+                        class="inline-block w-32 text-center text-teal-900 bg-gray-100 rounded-md border-transparent md:text-lg lg:text-xl focus:border-gray-500 focus:bg-white focus:ring-0"
                         type="number" min="0" step="1" value=10 wire:model="quantite"
                         wire:change.debounce = "maj"><span class="hidden md:inline md:text-lg lg:text-xl"> g/jour par
                         animal</span>
@@ -89,19 +88,19 @@
 
 
         <div id="mineral" class="my-3 lg:flex-auto">
-            <h2 class="h3 pl-2 py-1 lg:pl-0 lg:text-center bg-gray-300 text-teal-900">4 - Saisir les valeurs du minéral
+            <h2 class="py-1 pl-2 text-teal-900 bg-gray-300 h3 lg:pl-0 lg:text-center">4 - Saisir les valeurs du minéral
                 (mg/kg
                 ou ppm)</h2>
-            <table class="w-full my-3 table-auto border-collapse border">
-                <thead class="bg-gray-700 text-gray-100 p-2">
+            <table class="my-3 w-full border border-collapse table-auto">
+                <thead class="p-2 text-gray-100 bg-gray-700">
                     <tr>
-                        <td class="py-3 px-4 border-gray-200 border">
+                        <td class="px-4 py-3 border border-gray-200">
                             Eléments (besoins<span class="hidden md:inline"> en ppm</span>)
                         </td>
-                        <td class="py-3 px-2 border-gray-200 border text-center">
+                        <td class="px-2 py-3 text-center border border-gray-200">
                             Minéral
                         </td>
-                        <td class="py-3 px-2 border-gray-200 border text-center">
+                        <td class="px-2 py-3 text-center border border-gray-200">
                             Apports pour {{ $quantite }} g/j
                         </td>
                     </tr>
@@ -109,23 +108,23 @@
                 <tbody>
                     @foreach ($listeOligos as $oligo => $oligoelement)
                         <tr>
-                            <td class="py-3 px-4 border border-gray-800 ml-3">
+                            <td class="px-4 py-3 ml-3 border border-gray-800">
                                 {{ ucfirst($oligoelement) }}
                                 ({{ $besoins[$oligo] * $msi }})
                             </td>
-                            <td class="py-3  px-2 border border-gray-800 text-center">
+                            <td class="px-2 py-3 text-center border border-gray-800">
                                 <input id="{{ $oligo }}" name="{{ $oligo }}" type="number" min="0"
-                                    step="1" wire:model="min.{{ $oligo }}" class="text-center w-32"
+                                    step="1" wire:model="min.{{ $oligo }}" class="w-32 text-center"
                                     wire:change.debounce = "majMineral()">
                             </td>
                             <td class="py-3 px-2 border border-gray-800 text-center {{ $bilan[$oligo] }}">
                                 @if ($bilan[$oligo] == 'toxicite')
-                                    <i class="fa-solid fa-skull text-white"></i>
+                                    <i class="text-white fa-solid fa-skull"></i>
                                 @endif
                                 @if ($mineral[$oligo] == 0)
                                     -
                                 @else
-                                {{ round(($mineral[$oligo] * $quantite) / 1000, 2) }}
+                                    {{ round(($mineral[$oligo] * $quantite) / 1000, 2) }}
                                 @endif
                             </td>
 
@@ -133,21 +132,21 @@
                     @endforeach
                 </tbody>
             </table>
-            <div class="border-2 bg-gray-200 my-2 px-3 py-5 flex flex-row gap-4 justify-around">
+            <div class="flex flex-row gap-4 justify-around px-3 py-5 my-2 bg-gray-200 border-2">
                 <div class="flex flex-row gap-1">
-                    <div class=" w-5 h-5 equilibre"></div>
+                    <div class="w-5 h-5 equilibre"></div>
                     Equilibre
                 </div>
                 <div class="flex flex-row gap-1">
-                    <div class=" w-5 h-5 carence"></div>
+                    <div class="w-5 h-5 carence"></div>
                     Carence
                 </div>
                 <div class="flex flex-row gap-1">
-                    <div class=" w-5 h-5 exces"></div>
+                    <div class="w-5 h-5 exces"></div>
                     Excès
                 </div>
                 <div class="flex flex-row gap-1">
-                    <div class=" w-5 h-5 toxicite"></div>
+                    <div class="w-5 h-5 toxicite"></div>
                     Toxicité
                 </div>
             </div>
