b54fa0e4b4d3cf42b7301e02261938e878fedb00
michelange2008 <laravel>
2024-02-19 05:53:54 +0100

    modification affichage page présentation scop

--- resources/views/antikor/antikor-index.blade.php
+++ resources/views/antikor/antikor-index.blade.php
@@ -1,20 +1,25 @@
 <x-app-layout>
 
     <div class="px-12">
-        @foreach ($datas as $data)
-            <p class="p-3 pb-3 mt-4 mb-1 text-lg font-bold text-gray-100 rounded md:text-xl bg-vert-900">
-                {{ $data->titre }}
-            </p>
-            <div class="gap-4 md:grid md:grid-rows-1 md:grid-cols-3">
-                <img class="" src="{{ url('storage/img/antikor/' . $data->photo) }}" alt="">
-
-                <div class="col-span-2 text-justify">
-
-                    <p class="md:pb-3">{{ $data->texte->p1 }}</p>
-                    <p>{{ $data->texte->p2 }}</p>
+        <div class="lg:grid lg:grid-cols-3 lg:gap-3">
+            @foreach ($datas as $data)
+                <div
+                    class="flex flex-col justify-center p-3 pb-3 mb-1 text-lg font-bold text-gray-100 rounded lg:text-3xl bg-vert-900">
+                    <p>
+                        {{ $data->titre }}
+                    </p>
                 </div>
-            </div>
-        @endforeach
+                <div class="mb-3 lg:mb-0">
+                    <img class="" src="{{ url('storage/img/antikor/' . $data->photo) }}" alt="">
+
+                    <div class="col-span-2 text-justify">
+
+                        <p class="lg:pb-3">{{ $data->texte->p1 }}</p>
+                        <p>{{ $data->texte->p2 }}</p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 
 </x-app-layout>
