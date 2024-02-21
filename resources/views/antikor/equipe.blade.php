4713ac3be4923a5646c23ae4092b1670a3aeae1d
michelange2008 <laravel>
2024-02-21 18:43:46 +0100

    augm taille photo équipe

--- resources/views/antikor/equipe.blade.php
+++ resources/views/antikor/equipe.blade.php
@@ -6,7 +6,7 @@
                 class="flex flex-col items-center py-3 text-center
                     @if ($loop->last) col-span-2 @endif">
 
-                <img class="w-16" src="{{ url('storage/img/antikor/' . $membre['photo']) }}" alt="">
+                <img class="w-24" src="{{ url('storage/img/antikor/' . $membre['photo']) }}" alt="">
                 <p class="font-bold text-vert-900">{{ $membre['nom'] }} </p>
                 <p class="text-gray-700">{{ $membre['profession'] }}</p>
                 <p class="text-gray-700 nowrap">{{ $membre['experience'] }}</p>
