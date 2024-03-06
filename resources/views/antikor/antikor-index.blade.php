d9640a5d702fa287577fafe5839d7ef0d5fc236f
michelange2008 <laravel>
2024-03-06 06:59:33 +0100

    adaptation page who aux smartphones

--- resources/views/antikor/antikor-index.blade.php
+++ resources/views/antikor/antikor-index.blade.php
@@ -1,17 +1,34 @@
 <x-app-layout>
 
     <div class="my-3 select-none">
-        <div class="lg:grid lg:grid-cols-3 lg:gap-3">
+        <div class="lg:hidden">
             @foreach ($datas as $data)
-            <div class="mb-3 lg:mb-0">
-                <img class="" src="{{ url('storage/img/antikor/' . $data->photo) }}" alt="">
-
-                <div class="col-span-2 text-justify">
-
-                    <p class="lg:pb-3">{{ $data->texte->p1 }}</p>
-                    <p>{{ $data->texte->p2 }}</p>
+                <div class="flex flex-col gap-1 mb-3">
+                    <div
+                        class="flex flex-col justify-center p-2 text-xl font-bold text-center text-white border bg-vert-700 border-vert-700">
+                        <p>{{ $data->titre }}</p>
+                    </div>
+                    <div class="col-span-2 p-3 bg-gray-200">
+                        <p class="lg:pb-3">{{ $data->texte->p1 }}</p>
+                        <p>{{ $data->texte->p2 }}</p>
+                    </div>
+                    <div class="col-span-3">
+                        <img class="" src="{{ url('storage/img/antikor/' . $data->photo) }}" alt="">
+                    </div>
                 </div>
-            </div>
+            @endforeach
+        </div>
+        <div class="hidden lg:grid lg:grid-cols-3 lg:gap-3">
+            @foreach ($datas as $data)
+                <div class="mb-3 lg:mb-0">
+                    <img class="" src="{{ url('storage/img/antikor/' . $data->photo) }}" alt="">
+
+                    <div class="col-span-2 text-justify">
+
+                        <p class="lg:pb-3">{{ $data->texte->p1 }}</p>
+                        <p>{{ $data->texte->p2 }}</p>
+                    </div>
+                </div>
                 <div class="mb-1 rounded lg:relative">
                     <div class="hidden w-full h-full bg-orange-200 lg:block lg:absolute">
                         @includeIf('antikor.' . $data->plus)
@@ -23,6 +40,7 @@
 
                 </div>
             @endforeach
+
         </div>
     </div>
 
--- resources/views/antikor/formations.blade.php
+++ resources/views/antikor/formations.blade.php
@@ -2,5 +2,5 @@
 
     <p class="p-3 pb-1 text-2xl font-black text-center text-vert-900">Lieux de formation</p>
     <img class="px-8" src="{{ url('storage/img/antikor/carte.svg') }}" alt="Carte de France">
-    
+
 </div>
